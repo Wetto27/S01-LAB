63fc6915ca00556bd96ba266478b35fd0a85f5b8
Wellington <wellingtonhdds27@gmail.com>
2024-10-07 14:35:53 -0300

    Relatorio 7

---
+++ Relatorio7/Exercicio1.rb
@@ -0,0 +1,12 @@
+puts "Digite o primeiro número:"
+n1 = gets.chomp.to_f
+
+puts "Digite o segundo número:"
+n2 = gets.chomp.to_f
+
+puts "Digite o terceiro número:"
+n3 = gets.chomp.to_f
+
+media = (n1 + n2 + n3) / 3
+
+puts "A média é: #{media}"
